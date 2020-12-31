e6ef424f83e41476a3841186fa163a76dd3acbf5
Adit Bala <adityarbala@gmail.com>
2020-12-31 17:48:53 -0500

    fixed phasing through solid blocks

--- src/Player.java
+++ src/Player.java
@@ -8,7 +8,7 @@ public class Player {
     private double velX = 0;
     private double velY = 0;
     private final int margin = 1;
-    private final int GRAVITY = 1;
+    private final int GRAVITY = 2;
     private Block[][] map;
 
     public Player(int posX, int posY) {
@@ -21,59 +21,47 @@ public class Player {
     }
 
     public void move() {
-        //time to try out Physics
-        /*Block playerBlock = getCurrBlock(map);*/
         if (velX > 0) {
-            Point currPoint = new Point((int) (posX + velX + SIZE), (int) posY + SIZE);
-            if (!getCurrBlock(map, currPoint).isSolid()) {
-                /*System.out.println("Pos X:" +  posX + ", " + "Get X: " + (getCurrBlock(map, currPoint).getPixelCoords().getX()+Block.SIZE/2));*/
+            if (getCurrBlock(map, new Point((int) (posX + SIZE + velX + margin), (int) posY + SIZE)) != null && !getCurrBlock(map, new Point((int) (posX + velX + SIZE), (int) posY + SIZE)).isSolid()) {
                 posX += velX;
             }
         } else if (velX < 0) {
-            Point currPoint = new Point((int) (posX + velX + margin), (int) posY + SIZE);
-            if (!getCurrBlock(map, currPoint).isSolid()) {
-                    posX += velX;
+
+            if (getCurrBlock(map, new Point((int) (posX + velX + margin), (int) posY + SIZE)) != null && !getCurrBlock(map, new Point((int) (posX + velX + margin),posY + SIZE)).isSolid()) {
+                posX += velX;
             }
         }
 
         if(inAir()) {
             if(velY < 0) {
                 // margin could cause problems in the future : (
-                Point currPoint = new Point((posX), posY - margin);
+                Point currPoint = new Point((posX), posY - 5);
                 if (getCurrBlock(map, currPoint).isSolid()) {
                     velY = 0;
                 }
             }
             velY += GRAVITY;
-            if(getCurrBlock(map, new Point(posX, (int) (posY + SIZE + velY + margin))).getColor().equals(Color.black)){ {
+            if(!getCurrBlock(map, new Point(posX, (int) (posY + SIZE + velY + margin))).getColor().equals(Color.white) || !getCurrBlock(map, new Point(posX + SIZE, (int) (posY + SIZE + velY + margin))).getColor().equals(Color.white)){
                     Point gap = new Point(posX,  (posY + SIZE + margin));
                     Point bottom = getCurrBlock(map, new Point(posX, (int) (posY + SIZE + velY + margin))).getPixelCoords();
                     posY += bottom.getY() - gap.getY() + margin;
                     velY = 0;
-                }
+
             }
         }
-
         posY += velY;
 
-/*        if (sumAccel() != GRAVITY) {
-            posY += sumAccel();
-            accelY += 2;
-        }*/
+
+
+
     }
 
-
-    /*private double sumAccel() {
-        return accelY + GRAVITY;
-    }*/
-
     private boolean inAir() {
-        Point currPoint = new Point(posX, posY + SIZE + margin);
-        if (getCurrBlock(map, currPoint).getColor().equals(Color.white)) {
-            System.out.println("In Air");
-            return true;
-        } else {
+        if(getCurrBlock(map, new Point(posX, posY + SIZE + margin)).getColor().equals(Color.black) ||
+           getCurrBlock(map, new Point(posX + SIZE, posY + SIZE + margin)).getColor().equals(Color.black)) {
             return false;
+        } else {
+            return true;
         }
     }
 
@@ -108,7 +96,7 @@ public class Player {
         }
         if (key == KeyEvent.VK_W) {
             if (!inAir()) {
-                velY = -10;
+                velY = -22;
             }
         }
     }
